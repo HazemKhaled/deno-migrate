de16b3786ce13e5ae485f48fcb5bcde930fa8d00
jojomak13 <jojomak350@gmail.com>
2024-10-26 22:53:25 +0300

    Remove deepMerge

--- src/utils.ts
+++ src/utils.ts
@@ -88,15 +88,3 @@ export async function getAvailableOptions(
 
   return availableOptions;
 }
-
-export function deepMerge(target: any, source: any): any {
-    for (const key in source) {
-      if (source[key] instanceof Object && key in target) {
-        Object.assign(
-            source[key],
-            deepMerge(target[key], source[key])
-        );
-      }
-    }
-    return { ...target, ...source };
-  }
