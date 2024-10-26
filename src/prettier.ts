1b40916d721272ab20ddc81ae24b2728ee5c3b1c
Joseph Makram <jojomak350@gmail.com>
2024-10-26 03:08:05 +0300

    Create prettier.ts

---
+++ src/prettier.ts
@@ -0,0 +1,52 @@
+import { join } from "https://deno.land/std@0.211.0/path/mod.ts";
+import type { DenoConfigType, GlobalConfigType } from "./types.ts";
+
+// TODO:: need to add all supported types conversion 🚨
+// For more help: https://docs.deno.com/runtime/fundamentals/linting_and_formatting
+const configMap: Record<string, string> = {
+    tabWidth: "indentWidth",
+    semi: "semiColons",
+};
+
+const mapRules = (options: GlobalConfigType): GlobalConfigType => {
+    const resOptions: GlobalConfigType = {};
+
+    for (const item in options) {
+        if (Object.hasOwn(configMap, item)) {
+            resOptions[configMap[item]] = options[item];
+        } else {
+            resOptions[item] = options[item];
+        }
+    }
+
+    return resOptions;
+};
+export async function migratePrettierScripts({
+    workingDirectory,
+    existingDenoConfig,
+}: {
+    workingDirectory: string;
+    existingDenoConfig: DenoConfigType;
+}) {
+    try {
+        const configFilePath = join(workingDirectory, ".prettierrc");
+        const configFile = JSON.parse(await Deno.readTextFile(configFilePath));
+
+        return {
+            ...existingDenoConfig,
+            fmt: {
+                options: {
+                    ...mapRules(configFile || {}),
+                },
+            },
+        } as unknown as DenoConfigType;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("❌ Error migrating scripts:", error.message);
+        } else {
+            console.error("❌ Error migrating scripts:", error);
+        }
+
+        return existingDenoConfig;
+    }
+}
